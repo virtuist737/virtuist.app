05b41b665ea4acb2ae5cb2cb74c9086db8fee18f
Tanner Braden <128498901+Pianoman737@users.noreply.github.com>
2025-01-21 19:50:53 -0700

    json to db
    Created virtuist.db with virtue data. Deleted virtues-data.json. Updated index.html and sw.js accordingly.

--- sw.js
+++ sw.js
@@ -3,13 +3,15 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/styles.css',
-    '/virtues-data.json',
+    '/virtuist.db',
     '/assets/apple-touch-icon.png',
     '/assets/favicon-32x32.png',
     '/assets/favicon-16x16.png',
     '/assets/favicon.ico',
     '/assets/android-chrome-192x192.png',
-    '/assets/android-chrome-512x512.png'
+    '/assets/android-chrome-512x512.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.wasm'
 ];
 
 self.addEventListener('install', event => {
@@ -28,13 +30,11 @@ self.addEventListener('fetch', event => {
                 }
                 return fetch(event.request)
                     .then(response => {
-                        // Check if we received a valid response
                         if (!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
 
                         const responseToCache = response.clone();
-
                         caches.open(CACHE_NAME)
                             .then(cache => {
                                 cache.put(event.request, responseToCache);
